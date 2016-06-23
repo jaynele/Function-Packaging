a196e9d9e63fd81f919bacf9eae30250febc36fb
jaynele <1798805740@qq.com>
2016-06-23 10:12:48 +0800

    edit

--- composer.php
+++ composer.php
@@ -1,4 +1,4 @@
 <?php 
-//webserver problerm ,we can download in https://getcomposer.org/download/ get 
-//cmd  ,cd ..  ,php composer.phar
+//webserver problerm ,we can download in https://getcomposer.org/download/ 
+//cmd  ,cd ..  ,php composer.phar,在这之前配置php.exe环境路径是必须的.
 //then we can get it,let's try it...
